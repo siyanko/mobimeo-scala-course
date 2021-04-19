1d52b141c1cdca75bb28b9e070a4c68a4f8ee719
Philip Fieber <philip.fieber@mobimeo.com>
2021-04-19 17:38:13 +0200

    add metrics

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@
 object Main {
   def main(args: Array[String]): Unit = {
     // call at end of the world
-    BusinessLogic.program(SimpleDB, SimpleHafas, SimpleLogging, ???).f0()
+    BusinessLogic.program(SimpleDB, SimpleHafas, SimpleLogging, SimpleMetrics).f0()
   }
 }
 
