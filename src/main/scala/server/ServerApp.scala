e25c93f860c6c0f75a1d68b152e0818db85887b5
Philip Fieber <philip.fieber@mobimeo.com>
2021-04-28 11:14:31 +0200

    WIP: implement put

--- src/main/scala/server/ServerApp.scala
+++ src/main/scala/server/ServerApp.scala
@@ -8,14 +8,15 @@ import org.http4s.dsl.io._
 import org.http4s.implicits._
 import io.circe.generic.auto._
 import org.http4s.circe._
-import Domain.{Subscription, SubscriptionRequest}
+import Domain.{Subscription, SubscriptionRequest, PutRequest}
 
 import java.util.UUID
 import scala.concurrent.ExecutionContext.global
 
 object ServerApp extends IOApp {
 
-  implicit val decoder: EntityDecoder[IO, SubscriptionRequest] = jsonOf[IO, SubscriptionRequest]
+  implicit val decoderPost: EntityDecoder[IO, SubscriptionRequest] = jsonOf[IO, SubscriptionRequest]
+  implicit val decoderPut: EntityDecoder[IO, PutRequest] = jsonOf[IO, PutRequest]
 
   override def run(args: List[String]): IO[ExitCode] = {
     var storage: Map[String, Subscription] = Map.empty
@@ -26,7 +27,7 @@ object ServerApp extends IOApp {
       case DELETE -> Root / "hello" / name =>
         Ok(s"No hello for $name")
 
-      case req@POST -> Root / "subscriptions" => for {
+      case req @ POST -> Root / "subscriptions" => for {
         request <- req.as[SubscriptionRequest]
         maybeValidRequest = SubscriptionRequest.validate(request)
         _ <- IO(println(maybeValidRequest))
@@ -41,7 +42,20 @@ object ServerApp extends IOApp {
         }
       } yield result
 
-      case req @ PUT -> Root / "subscriptions" / id => ???
+      case req @ PUT -> Root / "subscriptions" / id => for {
+        request <- req.as[PutRequest]
+        maybeValidRequest = PutRequest.validate(request)
+        _ <- IO(println(maybeValidRequest))
+        _ <- IO(println(storage))
+        result <- maybeValidRequest match {
+          case None => BadRequest("Invalid body")
+          case Some(request) =>
+            if (!storage.contains(id)) BadRequest(s"Subscription with id $id does not exist")
+            val subscription = Subscription(id, request.origin, request.destination, request.label)
+            storage = Map(id -> subscription) ++ storage
+            Ok(s"""{"id": $id}""")
+        }
+      } yield result
     }.orNotFound
 
     BlazeServerBuilder[IO](global)
@@ -57,12 +71,17 @@ object ServerApp extends IOApp {
 
 object Domain {
   case class SubscriptionRequest(origin: String, destination: String, label: String)
-  case class PutRequest()
+  case class PutRequest(origin: String, destination: String, label: String)
 
   case class Subscription(id: String, origin: String, destination: String, label: String)
 
   object SubscriptionRequest {
     def validate(request: SubscriptionRequest): Option[SubscriptionRequest] = if (request.origin.nonEmpty && request.destination.nonEmpty && request.label.nonEmpty)
+      Some(request) else None
+  }
+
+  object PutRequest {
+    def validate(request: PutRequest): Option[PutRequest] = if (request.origin.nonEmpty && request.destination.nonEmpty && request.label.nonEmpty)
       Some(request) else None
   }
 }
